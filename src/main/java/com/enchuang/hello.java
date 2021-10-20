3a6269ac1b2cb0b49b9e9c52cc1192d7598940ce
wez <837322855@qq.com>
2021-10-20 18:18:09 +0800

    first commit

---
+++ src/main/java/com/enchuang/hello.java
@@ -0,0 +1,15 @@
+package com.enchuang;
+
+/**
+ * @author wez
+ * @desc
+ * @date 2021/10/20 - 18:08
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("dsadasd");
+        System.out.println("dsadasd");
+    }
+}
