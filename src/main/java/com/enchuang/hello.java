424b8c90765df07991ccc5cbf1a1c8eb45ea5780
wez <837322855@qq.com>
2021-10-20 18:48:20 +0800

    push commit

--- src/main/java/com/enchuang/hello.java
+++ src/main/java/com/enchuang/hello.java
@@ -6,12 +6,13 @@ package com.enchuang;
  * @date 2021/10/20 - 18:08
  */
 public class hello {
-
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("master!");
         System.out.println("hot-fix");
         System.out.println("dsadasd");
         System.out.println("dsadasd");
+        System.out.println("dsadasd");
+
     }
 }
