49dccfa8e12f8de5c891f001c5fc1674c8c47530
wez <837322855@qq.com>
2021-10-20 18:21:57 +0800

    second commit

--- src/main/java/com/enchuang/hello.java
+++ src/main/java/com/enchuang/hello.java
@@ -9,7 +9,5 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.println("helloworld");
-        System.out.println("dsadasd");
-        System.out.println("dsadasd");
     }
 }
